2ef99411b51ad8262e5989cad8c171f948667017
everstudybee <everstudybee@gmail.com>
2022-04-05 03:18:49 +0200

    3. Create and set up a Firebase project

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,13 +1,24 @@
 /**
  * To find your Firebase config object:
- * 
+ *
  * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
  * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+    apiKey: "AIzaSyBOdKQ9xOO54vxihYIpdRmaU40dChp1RzI",
+
+    authDomain: "friendly-chat-chris.firebaseapp.com",
+
+    projectId: "friendly-chat-chris",
+
+    storageBucket: "friendly-chat-chris.appspot.com",
+
+    messagingSenderId: "906124987723",
+
+    appId: "1:906124987723:web:91577b6bee6006ea3aacce"
+
 };
 
 export function getFirebaseConfig() {
